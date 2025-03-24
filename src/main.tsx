cb66ff51e8f928e33ba9647d6d693a6b4814f5b1
Dave Berner <davidajberner@gmail.com>
2025-03-24 11:36:28 +1100

    feat: remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <KindeProvider
-      clientId={import.meta.env.VITE_KINDE_CLIENT_ID}
-      domain={import.meta.env.VITE_KINDE_DOMAIN}
-      logoutUri={import.meta.env.VITE_KINDE_LOGOUT_URL}
-      redirectUri={import.meta.env.VITE_KINDE_REDIRECT_URL}
-    >
-      <App />
-    </KindeProvider>
-  </StrictMode>
+  <KindeProvider
+    clientId={import.meta.env.VITE_KINDE_CLIENT_ID}
+    domain={import.meta.env.VITE_KINDE_DOMAIN}
+    logoutUri={import.meta.env.VITE_KINDE_LOGOUT_URL}
+    redirectUri={import.meta.env.VITE_KINDE_REDIRECT_URL}
+    useInsecureForRefreshToken={true}
+  >
+    <App />
+  </KindeProvider>
 );
