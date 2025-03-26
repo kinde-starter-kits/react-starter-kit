2a89d4a8c90c630e4106ecd3e0734790d2b2a9bc
Daniel Rivers <me@danielrivers.com>
2025-03-26 18:06:35 +0000

    chore: bump version and add comment about useInsecureForRefreshToken

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,8 @@ createRoot(document.getElementById("root")!).render(
     domain={import.meta.env.VITE_KINDE_DOMAIN}
     logoutUri={import.meta.env.VITE_KINDE_LOGOUT_URL}
     redirectUri={import.meta.env.VITE_KINDE_REDIRECT_URL}
-    useInsecureForRefreshToken={true}
+    // When running local against a custom domain, include the line below
+    // useInsecureForRefreshToken={true}
   >
     <App />
   </KindeProvider>
