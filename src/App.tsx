9624e19d502659b0b8e0886623c9166d716175d8
Peter Phanouvong <peterphanouvong@gmail.com>
2025-08-15 12:23:16 +1000

    test: no auth by default

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
-import LoggedIn from "./components/LoggedIn";
-import LoggedOut from "./components/LoggedOut";
+// import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+// import LoggedIn from "./components/LoggedIn";
+// import LoggedOut from "./components/LoggedOut";
 
 export default function App() {
-  const { isLoading, isAuthenticated } = useKindeAuth();
+  // const { isLoading, isAuthenticated } = useKindeAuth();
 
-  if (isLoading) return <>Loading...</>;
+  // if (isLoading) return <>Loading...</>;
 
-  return isAuthenticated ? <LoggedIn /> : <LoggedOut />;
+  // return isAuthenticated ? <LoggedIn /> : <LoggedOut />;
+
+  return <div>Hello World</div>;
 }
